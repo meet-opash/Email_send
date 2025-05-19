2180caee7420f54d575232be618e44b58cd4643f
meet <ms.opash@gmail.com>
2025-05-19 17:33:23 +0530

    first commit

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,9 @@ const apiRoutes = require('./api');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  // const user = new usersModel({
-  // });
-  res.render('index', { title: 'Welcome to Demo API !!' });
+  res.json({ message: 'Welcome to Demo API !!' });
 });
+
 router.use('/api', apiRoutes);
 
 module.exports = router;
