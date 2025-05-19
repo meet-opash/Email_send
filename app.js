aee680f669e9129446ddfd33e850487900281bce
meet <ms.opash@gmail.com>
2025-05-19 17:35:51 +0530

    first commit

--- app.js
+++ app.js
@@ -11,13 +11,9 @@ const { connect } = require('./utils/database');
 const bodyParser = require("body-parser");
 var app = express();
 
-
-
-
 // view engine setup
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
-
 
 app.use(cors());
 app.use(logger('dev'));
@@ -27,9 +23,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 
-
+// Connect to database
 connect();
 
+// Routes
 app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 
@@ -41,13 +38,26 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-   console.error(err.stack);
+  console.error(err.stack);
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+    error: {
+      message: err.message,
+      status: err.status || 500
+    }
+  });
 });
+
+// Only start the server if this file is run directly
+if (require.main === module) {
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}
 
 module.exports = app;
